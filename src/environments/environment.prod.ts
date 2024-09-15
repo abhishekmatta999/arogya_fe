982e2a51447f98ea9dc63e28202223ec6a684f42
abhishek <59220090+abhishekmatta999@users.noreply.github.com>
2024-09-15 16:24:03 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: false,
+  API_BASE_PATH:"https://7d26-157-119-213-251.ngrok-free.app/api/v1/",
+  FILE_PATH:"https://7d26-157-119-213-251.ngrok-free.app",
+  clientId:
+    "690690739244-f3kc66ra4a4qquh2ei4knq63200a62v5.apps.googleusercontent.com",
 };
